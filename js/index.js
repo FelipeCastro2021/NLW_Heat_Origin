586cf99bce2410e4c5cc933bb2e7aa0fd70423b8
Felipe Castro 2021 <felipe2021castro@gmail.com>
2021-10-22 03:43:52 -0300

    Add integration with Github API

--- js/index.js
+++ js/index.js
@@ -23,3 +23,19 @@ function changeSocialMediaLinks() {
 console.log(socialLinks.children)
 
 changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url //Troca o valor da URL
+      userImage.src = data.avatar_url
+      userLogin.textContent = `@${data.login}`
+    })
+}
+
+getGithubProfileInfos()
